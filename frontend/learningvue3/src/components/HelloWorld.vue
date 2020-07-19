7769d8b502b2caef6f08d4a8ed36632713ab125b
Chad Smith <8794805+chadsmith12@users.noreply.github.com>
2020-07-19 13:32:49 -0500

    update learningvue3 example

--- frontend/learningvue3/src/components/HelloWorld.vue
+++ frontend/learningvue3/src/components/HelloWorld.vue
@@ -1,7 +1,12 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div class="demo" :title="titleMessage">
+    <span v-if="showMessage">
+      {{ msg }}
+    </span><br>
+    <label> Show Message
+    <input type="checkbox" v-model="showMessage">
+  </label>
+  </div>
 </template>
 
 <script>
@@ -12,8 +17,24 @@ export default {
   },
   data() {
     return {
-      count: 0
+      count: 0,
+      titleMessage: 'You loaded this page on ' + new Date().toLocaleString(),
+      showMessage: true
+    }
+  },
+  methods: {
+    add2() {
+      this.count+=2;
     }
   }
 }
 </script>
+
+<style scoped>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+}
+</style>
